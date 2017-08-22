e1a77558dc61fcbf1948f0c300a6a260ec8ac6e3
FOKO <miguelfoko@gmail.com>
2017-08-22 13:43:37 +0200

    RAS

--- frontend/src/main/frontend/src/pages/home/home.ts
+++ frontend/src/main/frontend/src/pages/home/home.ts
@@ -9,14 +9,14 @@ import { RestProvider } from '../../providers/rest/rest';
 export class HomePage {
 public name:string='rrr';
   constructor(public rest:RestProvider) {
-		this.getName();
+	//	this.getName();
   }
-  public getName(){
+ /* public getName(){
     let nom=this.name;
     this.rest.getHello().subscribe((data)=>{
 		console.log(data);
 		alert("name: "+data);
     	nom=data;
 	});
-  }
+  }*/
 }

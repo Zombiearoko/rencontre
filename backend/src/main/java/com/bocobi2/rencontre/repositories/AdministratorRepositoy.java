f26c639c9da949ba40ad9f4d7a2d384e8b4ecc2a
Volviane <sapphiremfogo@gmail.com>
2017-09-06 13:38:47 +0100

    modification de la classe administratorRepository

--- backend/src/main/java/com/bocobi2/rencontre/repositories/AdministratorRepositoy.java
+++ backend/src/main/java/com/bocobi2/rencontre/repositories/AdministratorRepositoy.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.bocobi2.rencontre.model.Administrator;
 
 public interface AdministratorRepositoy extends MongoRepository<Administrator, String> {
-	public Administrator findByAdminLogin(String adminLogin);
+	public Administrator findByLoginAdmin(String loginAdmin);
 
 }
